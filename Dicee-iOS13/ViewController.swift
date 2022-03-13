24a39c427b1a3616ada2ec0a2e5a5aec3a2b2ab8
PesAdam <pesadam15@gmail.com>
2022-03-13 14:11:22 +0100

    random

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,21 +12,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView! //vytvorenie prepojena frontendu s kodom
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 1
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-
-    
     @IBAction func rollButton(_ sender: UIButton) {
-        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
         
-        leftDiceNumber = leftDiceNumber + 1
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
         
     }
-    
-
-
 }
